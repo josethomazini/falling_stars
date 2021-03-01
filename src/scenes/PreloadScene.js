7067f9574070e85b8d11aa3863bc18d54d916eec
Jose Francisco Brand Thomazini <jose.thomazini@gmail.com>
2021-02-28 23:20:58 -0300

    Loaging bar

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -12,7 +12,7 @@ export default class PreloadScene extends Phaser.Scene {
     super('PreloadScene');
   }
 
-  preload() {
+  loadAssets() {
     this.load.image(keys.sky, sky);
     this.load.image(keys.platform, platform);
     this.load.image(keys.star, star);
@@ -25,6 +25,70 @@ export default class PreloadScene extends Phaser.Scene {
         frameHeight: 48,
       },
     );
+  }
+
+  preload() {
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const { width } = this.cameras.main;
+    const { height } = this.cameras.main;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    const assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', (value) => {
+      percentText.setText(`${parseInt(value * 100, 10)}%`);
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', (file) => {
+      assetText.setText(`Loading asset: ${file.key}`);
+    });
+
+    this.load.on('complete', () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
+    this.loadAssets();
   }
 
   create() {
